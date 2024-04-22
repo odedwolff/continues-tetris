01952098a401638e5d230693110dd501d19c4d5a
oded wolff <odedwolff@gmail.com>
2024-04-22 14:23:39 +0300

    added rand instantiation

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject testSphere;
-    // Start is called before the first frame update
+
+    public List<GameObject> myGameObjectList;   // Start is called before the first frame update
     void Start()
     {
-        Instantiate(testSphere);
+        int randomIndex = Random.Range(0, myGameObjectList.Count);
+        Instantiate(myGameObjectList[randomIndex]);
     }
 
     // Update is called once per frame
